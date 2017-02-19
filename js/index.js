1c6dd25993c66485522ae3285b96a0bfdef76ad6
limeng <limeng@qq.com>
2017-02-19 21:43:52 +0800

    index

--- js/index.js
+++ js/index.js
@@ -1,46 +1,25 @@
-window.onload = function () {
-    new Swiper("#swiper-container", {
-        speed: 500,
-        direction: "vertical",
-        mousewheelControl: !0,
-        onInit: function (a) {
-            a.myactive = 0;
-            for (var b = 0; b < a.slides.length; b++)
-                a.slides[b].style.zIndex = 0;
-            a.slides[a.myactive].style.zIndex = 1,
-                swiperAnimateCache(a),
-                swiperAnimate(a)
-        },
-        onTransitionStart: function (a) {
-            a.params.onlyExternal = !0
-        },
-        onTransitionEnd: function (a) {
-            a.params.onlyExternal = !1,
-                a.myactive = a.activeIndex;
-            for (var b = 0; b < a.slides.length; b++)
-                a.slides[b].style.zIndex = 0;
-            a.slides[a.myactive].style.zIndex = 1,
-                swiperAnimate(a)
-        },
-        watchSlidesProgress: !0,
-        onProgress: function (a) {
-            var b, c, d, e;
-            for (a.myactive || (a.myactive = 0),
-                     b = 0; b < a.slides.length; b++)
-                c = a.slides[b],
-                    es = c.style,
-                    d = c.progress,
-                    e = d * a.height,
-                    opacity = 0,
-                b == a.myactive && (opacity = 1 - Math.abs(d) / 2),
-                (a.slides[a.myactive].progress > 0 && b == a.myactive + 1 || a.slides[a.myactive].progress < 0 && b == a.myactive - 1) && (opacity = .5 + Math.abs(.5 * a.slides[a.myactive].progress)),
-                    c.style.opacity = opacity,
-                b != a.myactive && (es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = "translate3d(0," + e + "px,0)")
-        },
-        onSetTransition: function (a, b) {
-            for (var c = 0; c < a.slides.length; c++)
-                es = a.slides[c].style,
-                    es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = b + "ms"
-        }
-    })
-};
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    autoHeight: true,
+    loop: true
+});
+var startScroll, touchStart, touchCurrent;
+swiper.slides.on('touchstart', function (e) {
+    startScroll = this.scrollTop;
+    touchStart = e.targetTouches[0].pageY;
+}, true);
+swiper.slides.on('touchmove', function (e) {
+    touchCurrent = e.targetTouches[0].pageY;
+    var touchesDiff = touchCurrent - touchStart;
+    var slide = this;
+    var onlyScrolling =
+        ( slide.scrollHeight > slide.offsetHeight ) && //allow only when slide is scrollable
+        (
+            ( touchesDiff < 0 && startScroll === 0 ) || //start from top edge to scroll bottom
+            ( touchesDiff > 0 && startScroll === ( slide.scrollHeight - slide.offsetHeight ) ) || //start from bottom edge to scroll top
+            ( startScroll > 0 && startScroll < ( slide.scrollHeight - slide.offsetHeight ) ) //start from the middle
+        );
+    if (onlyScrolling) {
+        e.stopPropagation();
+    }
+}, true);
